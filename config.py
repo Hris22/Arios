b6b713e3a8244a889edcfadfe30655add41b291f
Hristiyan Baev <hristiqn_b@abv.bg>
2026-04-20 10:30:03 +0300

    cleared git cache

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "ac7556a46145af09fd19773d3871f8c5041af32c4c86acc99abda77d487cc493"
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    class Config:
-        env_file = ".env" # This will allow you to override with a .env file
+    class Config: 
+        env_file = ".env" # This will allow you to override with a .env file 
 
 settings = Settings()
